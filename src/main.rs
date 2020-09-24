c7e45ac3a63a38b6bc4439386505d037d466033d
windsfoot <22443508@qq.com>
2020-09-24 23:21:18 +0800

    开始学爬虫

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use futures;
+use futures::future;
+use futures::stream::{self, StreamExt};
+#[tokio::main]
+async fn main(){
+
+    let paths = vec![
+        "https://www.baidu.com".to_string(),
+        "https://www.baidu.com".to_string(),
+        "https://www.baidu.com".to_string(),
+        "https://www.baidu.com".to_string(),
+        "https://www.baidu.com".to_string(),
+        "https://www.baidu.com".to_string(),
+        "https://www.littley.top".to_string(),
+        "https://www.littley.top".to_string(),
+        "https://www.littley.top".to_string(),
+        "https://www.littley.top".to_string(),
+        "https://www.littley.top".to_string(),
+        "https://www.littley.top".to_string(),
+        "https://www.littley.top".to_string(),
+        "https://blog.csdn.net/".to_string(),
+        "https://blog.csdn.net/".to_string(),
+        "https://blog.csdn.net/".to_string(),
+        "https://blog.csdn.net/".to_string(),
+        "https://blog.csdn.net/".to_string(),
+        "https://blog.csdn.net/".to_string(),
+    ];
+    let fetches = futures::stream::iter(
+        paths.into_iter().map(|path| {
+            async move {
+                match reqwest::get(&path).await {
+                    Ok(resp) => {
+                        match resp.text().await {
+                            Ok(text) => {
+                                println!("RESPONSE: {} bytes from {}", text.len(), path);
+                            }
+                            Err(_) => println!("ERROR reading {}", path),
+                        }
+                    }
+                    Err(_) => println!("ERROR downloading {}", path),
+                }
+            }
+        })
+    ).buffer_unordered(8).collect::<Vec<()>>();
+    println!("Waiting...");
+    fetches.await;
 }
