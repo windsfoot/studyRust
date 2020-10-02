1148e77f4bcc0eef1b0f21384c801df60bdf2727
windsfoot <22443508@qq.com>
2020-10-02 23:01:20 +0800

    如何取得所有代码表

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use futures;
 use futures::future;
 use futures::stream::{self, StreamExt};
+use serde::{Deserialize, Serialize};
 use serde_json;
-use serde::{Serialize, Deserialize};
-#[derive(Debug)]
-#[derive(Serialize, Deserialize)]
- struct Symb {
+#[derive(Debug, Serialize, Deserialize)]
+struct Symb {
     symbol: String,
     code: String,
     name: String,
@@ -30,10 +29,8 @@ use serde::{Serialize, Deserialize};
 
 #[tokio::main]
 async fn main() {
-    let paths = vec![
-
-        "http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page=1&num=90&sort=symbol&asc=1&node=cyb&_s_r_a=init".to_string(),
-    ];
+    let aa = format!("http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page={}&num=80&sort=symbol&asc=1&node=cyb&_s_r_a=init", 1);
+    let paths = vec![aa];
     let fetches = futures::stream::iter(paths.into_iter().map(|path| async move {
         match reqwest::get(&path).await {
             Ok(resp) => match resp.text().await {
@@ -42,12 +39,10 @@ async fn main() {
                     let v: Vec<serde_json::Value> = serde_json::from_str(&text).unwrap();
                     //println!("{:?}",v);
                     for i in v {
-                      //  let (vv:Symb,er) = serde_json::from_value(i);
-                      if let  Some(k)=i.get("symbol"){
-                        println!("{}\n", k);
-                      };
-                        
-                        
+                        //  let (vv:Symb,er) = serde_json::from_value(i);
+                        if let Some(k) = i.get("symbol") {
+                            println!("{}\n", k);
+                        };
                     }
 
                     //let v1:Vec::<serde_json::Value>=v;
