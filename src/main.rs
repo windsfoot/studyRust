6f2343959afb2d548825883a8fd4fd6953e7b424
windsfoot <22443508@qq.com>
2020-10-02 20:12:29 +0800

    serde无法直接解析为结构，value结构自行解析。

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
-use crate::si::si::Symb;
 use futures;
 use futures::future;
 use futures::stream::{self, StreamExt};
 use serde_json;
-mod si;
-
+use serde::{Serialize, Deserialize};
+#[derive(Debug)]
+#[derive(Serialize, Deserialize)]
+ struct Symb {
+    symbol: String,
+    code: String,
+    name: String,
+    trade: f32,
+    pricechange: f32,
+    changepercent: f32,
+    buy: f32,
+    sell: f32,
+    settlement: f32,
+    open: f32,
+    high: f32,
+    low: f32,
+    volume: f64,
+    amount: f64,
+    ticktime: String,
+    per: f32,
+    pb: f32,
+    mktcap: f64,
+    nmc: f64,
+    turnoverratio: f64,
+}
 
 #[tokio::main]
 async fn main() {
@@ -20,13 +42,17 @@ async fn main() {
                     let v: Vec<serde_json::Value> = serde_json::from_str(&text).unwrap();
                     //println!("{:?}",v);
                     for i in v {
-                       let vv: serde_json::Value     = serde_json::from_value(i).unwrap();
-                        println!("{:?}\n", vv);
-                        break;
+                      //  let (vv:Symb,er) = serde_json::from_value(i);
+                      if let  Some(k)=i.get("symbol"){
+                        println!("{}\n", k);
+                      };
+                        
+                        
                     }
 
                     //let v1:Vec::<serde_json::Value>=v;
                 }
+
                 Err(_) => println!("ERROR reading {}", path),
             },
             Err(_) => println!("ERROR downloading {}", path),
--- src/si.rs
+++ src/si.rs
@@ -1,5 +1,7 @@
 //{"symbol":"sz300001","code":"300001","name":"\u7279\u9510\u5fb7","trade":"17.650","pricechange":-0.15,"changepercent":-0.843,"buy":"17.650","sell":"17.660","settlement":"17.800","open":"17.840","high":"17.960","low":"17.500","volume":7221217,"amount":127975579,"ticktime":"16:30:00","per":65.37,"pb":4.034,"mktcap":1760711.182375,"nmc":1667436.901605,"turnoverratio":0.76437}
+use serde_json;
 pub mod si {
+    #[derive(Serialize, Deserialize)]
     pub struct Symb {
         symbol: String,
         code: String,
