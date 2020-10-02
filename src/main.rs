c73f526941a50a0d1a3b3c7664c24e931f88fdb1
windsfoot <22443508@qq.com>
2020-10-02 16:43:18 +0800

    serde_json 解析搞不定

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,39 @@
+use crate::si::si::Symb;
 use futures;
 use futures::future;
 use futures::stream::{self, StreamExt};
-#[tokio::main]
-async fn main(){
+use serde_json;
+mod si;
 
+
+#[tokio::main]
+async fn main() {
     let paths = vec![
-        "https://www.baidu.com".to_string(),
-        "https://www.baidu.com".to_string(),
-        "https://www.baidu.com".to_string(),
-        "https://www.baidu.com".to_string(),
-        "https://www.baidu.com".to_string(),
-        "https://www.baidu.com".to_string(),
-        "https://www.littley.top".to_string(),
-        "https://www.littley.top".to_string(),
-        "https://www.littley.top".to_string(),
-        "https://www.littley.top".to_string(),
-        "https://www.littley.top".to_string(),
-        "https://www.littley.top".to_string(),
-        "https://www.littley.top".to_string(),
-        "https://blog.csdn.net/".to_string(),
-        "https://blog.csdn.net/".to_string(),
-        "https://blog.csdn.net/".to_string(),
-        "https://blog.csdn.net/".to_string(),
-        "https://blog.csdn.net/".to_string(),
-        "https://blog.csdn.net/".to_string(),
+
+        "http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page=1&num=40&sort=symbol&asc=1&node=cyb&_s_r_a=init".to_string(),
     ];
-    let fetches = futures::stream::iter(
-        paths.into_iter().map(|path| {
-            async move {
-                match reqwest::get(&path).await {
-                    Ok(resp) => {
-                        match resp.text().await {
-                            Ok(text) => {
-                                println!("RESPONSE: {} bytes from {}", text.len(), path);
-                            }
-                            Err(_) => println!("ERROR reading {}", path),
-                        }
+    let fetches = futures::stream::iter(paths.into_iter().map(|path| async move {
+        match reqwest::get(&path).await {
+            Ok(resp) => match resp.text().await {
+                Ok(text) => {
+                    //println!("{:?}",text);
+                    let v: Vec<serde_json::Value> = serde_json::from_str(&text).unwrap();
+                    //println!("{:?}",v);
+                    for i in v {
+                       let vv: serde_json::Value     = serde_json::from_value(i).unwrap();
+                        println!("{:?}\n", vv);
+                        break;
                     }
-                    Err(_) => println!("ERROR downloading {}", path),
+
+                    //let v1:Vec::<serde_json::Value>=v;
                 }
-            }
-        })
-    ).buffer_unordered(8).collect::<Vec<()>>();
+                Err(_) => println!("ERROR reading {}", path),
+            },
+            Err(_) => println!("ERROR downloading {}", path),
+        }
+    }))
+    .buffer_unordered(8)
+    .collect::<Vec<()>>();
     println!("Waiting...");
     fetches.await;
 }
---
+++ src/si.rs
@@ -0,0 +1,25 @@
+//{"symbol":"sz300001","code":"300001","name":"\u7279\u9510\u5fb7","trade":"17.650","pricechange":-0.15,"changepercent":-0.843,"buy":"17.650","sell":"17.660","settlement":"17.800","open":"17.840","high":"17.960","low":"17.500","volume":7221217,"amount":127975579,"ticktime":"16:30:00","per":65.37,"pb":4.034,"mktcap":1760711.182375,"nmc":1667436.901605,"turnoverratio":0.76437}
+pub mod si {
+    pub struct Symb {
+        symbol: String,
+        code: String,
+        name: String,
+        trade: f32,
+        pricechange: f32,
+        changepercent: f32,
+        buy: f32,
+        sell: f32,
+        settlement: f32,
+        open: f32,
+        high: f32,
+        low: f32,
+        volume: f64,
+        amount: f64,
+        ticktime: String,
+        per: f32,
+        pb: f32,
+        mktcap: f64,
+        nmc: f64,
+        turnoverratio: f64,
+    }
+}
