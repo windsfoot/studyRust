3fe281301468ccda29eecad22a3281d1837710e9
windsfoot <22443508@qq.com>
2020-10-03 10:52:41 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,6 @@ use futures::future;
 use futures::stream::{self, StreamExt};
 use serde::{Deserialize, Serialize};
 use serde_json;
-#[derive(Debug, Serialize, Deserialize)]
-struct Symb {
-    symbol: String,
-    code: String,
-    name: String,
-    trade: f32,
-    pricechange: f32,
-    changepercent: f32,
-    buy: f32,
-    sell: f32,
-    settlement: f32,
-    open: f32,
-    high: f32,
-    low: f32,
-    volume: f64,
-    amount: f64,
-    ticktime: String,
-    per: f32,
-    pb: f32,
-    mktcap: f64,
-    nmc: f64,
-    turnoverratio: f64,
-}
 
 #[tokio::main]
 async fn main() {
