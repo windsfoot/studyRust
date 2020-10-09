71977baf905a0a97a0c39aaeb507705ec0bd60cc
windsfoot <22443508@qq.com>
2020-10-09 16:17:47 +0800

    结构名改成Sina，在结构内部取得股票代码表，下午解决循环。

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ mod sina;
 
 
 
-use crate::sina::sina::HttpAdress;
+use crate::futures::executor::block_on;
+use crate::sina::sina::Sina;
 use futures;
 use futures::future;
 use futures::stream::{self, StreamExt};
@@ -14,36 +15,13 @@ use serde_json;
 
 #[tokio::main]
 async fn main() {
-    let mut c:HttpAdress=HttpAdress{dress:String::from("")};
-    c.set_dress(1,10,"cyb");
-    let aa =String::from( c.get());
-    let paths = vec![aa];
-    let fetches = futures::stream::iter(paths.into_iter().map(|path| async move {
-        match reqwest::get(&path).await {
-            Ok(resp) => match resp.text().await {
-                Ok(text) => {
-                    //println!("{:?}",text);
-                    let v: Vec<serde_json::Value> = serde_json::from_str(&text).unwrap();
-                    //println!("{:?}",v);
-                    for i in v {
-                        //  let (vv:Symb,er) = serde_json::from_value(i);
-                        if let Some(k) = i.get("symbol") {
-                            println!("{}\n", k);
-                        };
-                    }
-
-                    //let v1:Vec::<serde_json::Value>=v;
-                }
-
-                Err(_) => println!("ERROR reading {}", path),
-            },
-            Err(_) => println!("ERROR downloading {}", path),
-        }
-    }))
-    .buffer_unordered(8)
-    .collect::<Vec<()>>();
-    println!("Waiting...");
-    fetches.await;
+    let mut c:Sina=Sina::new();
+ 
+   let cc=c.set_dress(1, 10, c.mkt[0]);
+   println!("{:?}",cc);
+   let future=c.get_symbol();
+   block_on(future);
+   println!("{:?}",c.symbol);
 
     
 }
--- src/sina.rs
+++ src/sina.rs
@@ -3,36 +3,45 @@ use reqwest;
 //从新浪获取行情数据
 pub mod sina {
     //新浪行情网址
-    pub struct HttpAdress {
-       symbol:String,
-       mkt:String
-        
+    pub struct Sina<'a> {
+        pub symbol: Vec<String>,
+        pub mkt: [&'a str; 4],
     }
 
     //找到解答，解决字符串参数格式化的方法：
     //1.定义一个小函数
     //2.定义一个宏  eg：macro_rules! hello {() => ("hello")};println!(hello!());
     //
-    impl HttpAdress {
-        pub fn set_dress(&mut self, x: i32, y: i32, z: &str)-> String{
+    impl Sina<'_> {
+        pub fn new() -> Self {
+            return Sina {
+                mkt: ["sh", "sz", "cyb", "kcb"],
+                symbol: vec![],
+            };
+        }
+        //抓取代码的链接控制
+        pub fn set_dress(&mut self, x: i32, y: i32, z: &str) -> String {
             return format!("http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page={}&num={}&sort=symbol&asc=1&node={}&_s_r_a=init",x,y,z);
         }
-    
-         pub async fn get_symbol(mut self) {
-            let s:String=self.set_dress(1, 10, "cyb");
+        //抓取代码表
+        pub async fn get_symbol(&mut self) {
+            let s: String = self.set_dress(100, 60, "cyb");
             match reqwest::get(&s).await {
                 Ok(resp) => match resp.text().await {
                     Ok(text) => {
                         let v: Vec<serde_json::Value> = serde_json::from_str(&text).unwrap();
+                        self.symbol.push(String::from("Start"));
                         for i in v {
-                            //  let (vv:Symb,er) = serde_json::from_value(i);
                             if let Some(k) = i.get("symbol") {
-                                println!("{}\n", k);
+                                if let serde_json::Value::String(j) = k {
+                                    self.symbol.push(j.to_string());
+                                }
                             }
                         }
                     }
                     Err(_) => println!("ERROR reading {}", s),
                 },
+               
                 Err(_) => println!("ERROR downloading {}", s),
             }
         }
