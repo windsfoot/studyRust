7f133854cc6f5f7d80d2ab5da08934ed5d7d3a8a
windsfoot <22443508@qq.com>
2020-10-30 23:52:25 +0800

    一次读入后处理比磁盘顺序读取处理快1秒 需要解析数据

--- src/tdx.rs
+++ src/tdx.rs
@@ -8,15 +8,27 @@ impl Tdx {
     pub fn new() -> Self {
         return Tdx {};
     }
+
     pub fn read_iday(self) {
         let mut buffer = [0u8; 4];
-        let mut file = fs::File::open("sz000001.day").unwrap();
-        for _ in 0..5{
-        file.read(&mut buffer).unwrap();
-        unsafe {
-            let c = mem::transmute::<[u8; 4], u32>(buffer);
-            println!("{:?}", c);
-        }
+        let tbuf = fs::read("sz000001.day");
+        match tbuf {
+            Ok(total) => {
+                let i = total.len() / 4;
+                let mut j = 0;
+                while j < i {
+                    buffer = [
+                        total[j * 4 + 0],
+                        total[j * 4 + 1],
+                        total[j * 4 + 2],
+                        total[j * 4 + 3],
+                    ];
+                    println!("{:?}", u32::from_le_bytes(buffer));
+                    j+=1;
+                }
+
+            }
+            Err(er) => println!("er with {:?}", er),
+        };
     }
-}
 }
