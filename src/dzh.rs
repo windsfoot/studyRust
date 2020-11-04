b5e103d7069ec7342ab9011b6bb4498e1b4859de
windsfoot <22443508@qq.com>
2020-11-04 16:45:25 +0800

    获取更多pwr文件后组合

--- src/dzh.rs
+++ src/dzh.rs
@@ -1,12 +1,11 @@
-use bytes::Bytes;
+use bytes::{Buf, Bytes};
+use chrono::{DateTime, Local, TimeZone};
 use encoding_rs::{GB18030, GBK};
 use reqwest;
 use std::collections::BTreeMap;
 use std::fs;
 use std::str;
 use std::time;
-use chrono::{Local, DateTime, TimeZone};
-use bytes::Buf;
 
 //大智慧除权文件读取 split.pwr
 // 除权文件网址 http://filedown.gw.com.cn/download/FIN/full_sh.FIN
@@ -34,48 +33,57 @@ impl Pwr<'_> {
         };
     }
 
-    //获取大智慧pwr文件并切头合并
+    //获取大智慧pwr文件
     pub async fn get_pwr_web(&mut self) {
         match reqwest::get(DZH_PWR_DRESS).await {
             Ok(resp) => match resp.bytes().await {
                 Ok(text) => {
                     self.pwrbuf = text;
-                    //   let (cow, encoding_used, errors) = GB18030.decode(self.pwrbuf.as_ref());
-                    //  println!("{:?}{:?}",encoding_used,errors);
                 }
                 Err(_) => {}
             },
-            Err(er) => {error!("{}",er);}
+            Err(er) => {
+                error!("{}", er);
+            }
         }
     }
     pub fn parse_pwr(&mut self) {
-        let mut p = 8;
-
+        let p = 8;
         let i = self.pwrbuf.len() - 8;
         if i % 120 == 0 {
             let j = i / 120;
             let mut k = 0;
             while k < j {
-                let readbuf = self.pwrbuf.slice(p+k*120..p + 120*(k+1));
-                
-                let f=&readbuf.slice(0..4);
-                if f==&Bytes::from(&b"\xff\xff\xff\xff"[..]){
-                    let a=readbuf.slice(4..12);
-                    println!("{:?}",a);
-                }else{
-                    
-                    let ubuf=[f.as_ref()[0],f.as_ref()[1],f.as_ref()[2],f.as_ref()[3],0,0,0,0];
-                    
+                let readbuf = self.pwrbuf.slice(p + k * 120..p + 120 * (k + 1));
+
+                let f = &readbuf.slice(0..4);
+                if f == &Bytes::from(&b"\xff\xff\xff\xff"[..]) {
+                    let a = readbuf.slice(4..12);
+                    println!("{:?}", a);
+                } else {
+                    let ubuf = [
+                        f.as_ref()[0],
+                        f.as_ref()[1],
+                        f.as_ref()[2],
+                        f.as_ref()[3],
+                        0,
+                        0,
+                        0,
+                        0,
+                    ];
                     let dt: DateTime<Local> = Local.timestamp(i64::from_le_bytes(ubuf), 0);
-                    let sg=readbuf.slice(4..8).get_f32_le();
-                    let pg=readbuf.slice(8..12).get_f32_le();
-                    let pgj=readbuf.slice(12..16).get_f32_le();
-                    let fh=readbuf.slice(16..20).get_f32_le();
-                    println!("日期{:?}送股{:?}配股{:?}配股价{:?}分红{:?}",dt,sg,pg,pgj,fh);
+                    let sg = readbuf.slice(4..8).get_f32_le();
+                    let pg = readbuf.slice(8..12).get_f32_le();
+                    let pgj = readbuf.slice(12..16).get_f32_le();
+                    let fh = readbuf.slice(16..20).get_f32_le();
+                    println!(
+                        "日期{:?}送股{:?}配股{:?}配股价{:?}分红{:?}",
+                        dt, sg, pg, pgj, fh
+                    );
                 }
-               //let (cow, encoding_used, errors) = GB18030.decode(readbuf.as_ref());
-               //println!("{:?}",f);
-                k+=1;
+                //let (cow, encoding_used, errors) = GB18030.decode(readbuf.as_ref());
+                //println!("{:?}",f);
+                k += 1;
             }
         } else {
             error!("pwr文件长度有误");
